862129033050b6267b648ac5b44408dbe5be7066
Tatsunori <tatsunori@uds.com.br>
2019-11-03 14:37:34 -0300

    alterado nomne do jar

--- src/test/java/com/CreditCalculator/creditCalculator/CreditCalculatorControllerApplicationTests.java
+++ src/test/java/com/CreditCalculator/creditCalculator/CreditCalculatorControllerApplicationTests.java
@@ -6,12 +6,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class CreditCalculatorControllerApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void test_shouldReturnZeroLimit_WhenLowValues() {
 		CreditLimitModel creditRequest = new CreditLimitModel(
 				"1",
 				"1",
@@ -21,6 +22,36 @@ class CreditCalculatorControllerApplicationTests {
 		);
 
 		assertEquals(0, CalculatorController.calculateCreditLimit(creditRequest));
+	}
+
+	@Test
+	void test_shoulReturnAMediumLimit_WhenMediumValues() {
+		CreditLimitModel mediumCreditValues = new CreditLimitModel(
+				"10000",
+				"10000",
+				"5",
+				"300000",
+				"700"
+		);
+
+		Double creditValue = CalculatorController.calculateCreditLimit(mediumCreditValues);
+		assertTrue(creditValue > 5000);
+		assertTrue(creditValue < 16000);
+	}
+
+	@Test
+	void test_shouldReturnAHighLimit_WhenHighValues() {
+		CreditLimitModel hightCreditValues = new CreditLimitModel(
+				"17000",
+				"17000",
+				"8",
+				"600000",
+				"975"
+		);
+
+		Double creditValue = CalculatorController.calculateCreditLimit(hightCreditValues);
+		assertTrue(creditValue > 14000);
+		assertTrue( creditValue < 30000);
 	}
 
 }
